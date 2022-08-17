d973bc8b52ce34fa796ee0ff36433000386732aa
saedMuhamed <saedxabo@gmail.com>
2022-08-17 18:58:37 +0300

    addidng env file to project

--- server/server.js
+++ server/server.js
@@ -1,7 +1,11 @@
+require("dotenv").config();
 const express = require("express");
-
 const app = express();
 
-app.listen(4000, () => {
+app.get("/", (req, res) => {
+  res.json({ mssg: "It work" });
+});
+
+app.listen(process.env.PORT, () => {
   console.log("App Working at port 4000");
 });
